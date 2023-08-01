bcafd4f50dc9b926113457b3354ca67b89f75633
jonhatanh <jonhatan.higuera@gmail.com>
2023-08-01 13:33:40 -0600

    Add app styles

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
+const libraryDiv = document.getElementById('library');
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function Library() {
+    this.books = [];
+}
+
+//Returns index
+Library.prototype.addBookToLibrary = function(book) {
+    return (this.books.push(book) - 1);
+}
+
+
+function Book(title, author, pages, read, url = null) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.url = url;
 }
 Book.prototype.info = function() {
-    return `${this.title} by ${author}, ${pages} pages, ${this.read ? 'readed' : 'not read yet'}`
+    return `${this.title} by ${author}, ${pages} pages, ${this.read ? 'read' : 'not read yet'}`;
 }
+
