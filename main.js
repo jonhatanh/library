dfd2909e5178272831a6e0afcf7e696b175570dd
jonhatanh <jonhatan.higuera@gmail.com>
2023-08-01 20:57:24 -0600

    Add functionality for statusButton and Delete

--- main.js
+++ main.js
@@ -2,11 +2,30 @@
 function Library() {
     this.books = [];
 }
-
 Library.prototype.container = document.getElementById('library');
+
+Library.prototype.changeBookStatus = function(book) {
+    if(book.status.toLowerCase() === 'unread') {
+        book.status = 'Reading';
+    } else if(book.status.toLowerCase() === 'reading') {
+        book.status = 'Read';
+    } else {
+        book.status = 'Unread';
+    }
+    document.querySelector(`button[data-title="${book.title}"].status`).innerText = book.status
+}
+Library.prototype.deleteBook = function(book) {
+    const bookIndex = this.books.indexOf(book);
+    this.books.splice(bookIndex, 1);
+    document.querySelector(`div[data-title="${book.title}"].book`).remove();
+}
+Library.prototype.getBookByTitle = function(bookTitle) {
+    return this.books.find(book => book.title === bookTitle)
+}
+
 //Returns index
 Library.prototype.addBookToLibrary = function(book) {
-    return (this.books.push(book) - 1);
+    this.books.push(book);
 }
 Library.prototype.showBooks = function() {
     this.books.forEach(book => !book.onScreen && this.addBookToView(book));
@@ -28,7 +47,7 @@ Library.prototype.addBookToView = function(book) {
     
     // Properties
     bookContainer.classList.add('book');
-    bookContainer.setAttribute('data-name',book.title);
+    bookContainer.setAttribute('data-title',book.title);
     bookImageContainer.classList.add('image');
     bookImage.src = book.url ?? './assets/default.jpg';
     bookImage.alt = 'Book Photo';
@@ -40,8 +59,11 @@ Library.prototype.addBookToView = function(book) {
 
     bookOptionsContainer.classList.add('options');
     bookStatusButton.innerText = book.status;
+    bookStatusButton.setAttribute('data-title',book.title);
+    bookStatusButton.classList.add('status');
     bookDeleteButton.innerText = 'Delete';
-    bookDeleteButton.setAttribute('data-name',book.title);
+    bookDeleteButton.setAttribute('data-title',book.title);
+    bookDeleteButton.classList.add('delete');
 
     //DOM
     bookImageContainer.appendChild(bookImage);
@@ -92,19 +114,17 @@ openModalBtn.addEventListener('click', (e)=> {
     bookForm.classList.add('show');
 })
 closeModalBtn.addEventListener('click', (e)=> {
-    // modal.classList.remove('show');
     modal.classList.add('hidde');
-    // bookForm.classList.remove('show');
     bookForm.classList.add('hidde');
 })
 modal.addEventListener('animationend', (e) => {
-    console.log(e);
     if(e.animationName !== 'closeModal') return;
     modal.classList.remove('show');
     bookForm.classList.remove('show');
     modal.classList.remove('hidde');
     bookForm.classList.remove('hidde');
 })
+
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = getFormData(e.target);
@@ -127,7 +147,7 @@ function getFormData(form) {
         author: form.author.value.trim(),
         pages: form.pages.value,
         status: form.status.value,
-        cover: form.cover.value.trim() == "" ? null : form.cover.value.trim(),
+        cover: form.cover.value.trim() === "" ? null : form.cover.value.trim(),
     }
     return data;
 }
@@ -136,9 +156,17 @@ function getFormData(form) {
 
 
 
-
 const library = new Library();
+console.log(library);
 
+
+document.getElementById('library').addEventListener('click', function(e) {
+    if(e.target.tagName !== 'BUTTON') return;
+    const book = library.getBookByTitle(e.target.dataset.title);
+    e.target.classList.contains('status') 
+        ? library.changeBookStatus(book)
+        : library.deleteBook(book);
+})
 
 library.addBookToLibrary(new Book('Eloquent JavaScript', 'Marijin Haverbeke', 17, 'Unread', 'https://eloquentjs-es.thedojo.mx/img/cover.jpg'))
 library.addBookToLibrary(new Book('Kimetsu no Yaiba', 'Koyoharu Gotōge', 23, 'Reading', 'https://i.pinimg.com/736x/e0/51/9a/e0519a6134d90926021cc40269cae405.jpg'))
